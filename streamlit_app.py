8a82cdafa578e44e3202a419c76ca51843345414
Aashish Kumar <ashishmanadl987@gmail.com>
2026-04-13 18:24:29 +0530

    Re-add API call for watermelon fruit data
    Re-import requests and fetch watermelon data from API.

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,9 +2,6 @@
 import streamlit as st
 from snowflake.snowpark.functions import col
 
-import requests  
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-
 # Write directly to the app.
 st.title(":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
 st.write(
@@ -49,3 +46,9 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
         
     st.success('Your Smoothie is ordered!', icon="✅")
+
+import requests  
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+st.text(smoothiefroot_response)
+
+
